7bb8bb2298de5a329bf58fbca78d4cf32e10cf96
Benoît Legat <benoit.legat@gmail.com>
2025-02-13 15:01:51 +0100

    Fix indent

--- MyUtils/src/clang.jl
+++ MyUtils/src/clang.jl
@@ -1,10 +1,10 @@
 import Clang_jll
 
 function compile_and_run(code, args = String[])
-	path = mktempdir()
-	main_file = joinpath(path, "main.c")
-	bin_file = joinpath(path, "bin")
-	write(main_file, """
+    path = mktempdir()
+    main_file = joinpath(path, "main.c")
+    bin_file = joinpath(path, "bin")
+    write(main_file, """
 #include <stdio.h>
 int main(int argc, char **argv) {
   for(int i=0; i<argc; i++) {
@@ -12,13 +12,13 @@ int main(int argc, char **argv) {
   }
 }
 """)
-	Clang_jll.clang() do exe
+    Clang_jll.clang() do exe
     	run(`$exe $main_file -o $bin_file`)
-	end
-	cmd = Cmd([bin_file; args])
+    end
+    cmd = Cmd([bin_file; args])
     # Ça serait pas mal d'aussi afficher le code C avec syntax highlighting. Est-ce que c'est possible facilement avec Clang ?
-	if !isempty(args)
-		println("\$ $(string(cmd)[2:end-1])") # `2:end-1` to remove the backsticks
-	end
-	run(cmd)
+    if !isempty(args)
+        println("\$ $(string(cmd)[2:end-1])") # `2:end-1` to remove the backsticks
+    end
+    run(cmd)
 end
