390d77884f0bfed587b6e0b178c1760dbda68e3a
Matthieu Baerts <matthieu.baerts@uclouvain.be>
2026-02-17 12:20:45 +0100

    C code: format using clang-format
    If we tell the students to write nicely formatted code, better to do the
    same here :)
    
    I didn't do the last two lectures because the code blocks are formatted
    differently.
    
    I simply used 'clang-format' and 'clang-format-docs' [1] (with a few
    'sed' and 'git add -p').
    
    Link: https://github.com/finsberg/clang-format-docs [1]
    Signed-off-by: Matthieu Baerts <matthieu.baerts@uclouvain.be>

--- examples/out_of_bounds.c
+++ examples/out_of_bounds.c
@@ -1,13 +1,14 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-  int i = 0;
-  int a[2];
-  a[-1] = 1000000;
-  a[2] = -1000000;
-  printf("&i = %p\n", &i);
-  printf("&a = %p\n", &a);
-  printf("i = %d\n", i);
-  return EXIT_SUCCESS;
+int main()
+{
+    int i = 0;
+    int a[2];
+    a[-1] = 1000000;
+    a[2] = -1000000;
+    printf("&i = %p\n", &i);
+    printf("&a = %p\n", &a);
+    printf("i = %d\n", i);
+    return EXIT_SUCCESS;
 }
