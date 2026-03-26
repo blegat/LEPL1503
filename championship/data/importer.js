ee495a5cf244d7598726ff524ce858f0e7099c11
Benoît Legat <benoit.legat@gmail.com>
2026-03-26 16:04:50 +0100

    Move to data folder

--- championship/data/importer.js
+++ championship/data/importer.js
@@ -1,1 +1,4 @@
-export default [  ];
+import results0 from "./2026_03_26.js";
+import results1 from "./data.js";
+
+export default [ results0, results1 ];
--- championship/importer.js
+++
@@ -1,4 +0,0 @@
-import results0 from "./2026_03_26.js";
-import results1 from "./data.js";
-
-export default [ results0, results1 ];
