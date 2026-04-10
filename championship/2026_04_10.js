1141b47554adada49f9ef6b619850a875df833f7
countermatt <matthieu.pigaglio@uclouvain.be>
2026-04-10 10:47:09 +0200

    2026_04_10

---
+++ championship/2026_04_10.js
@@ -0,0 +1,18 @@
+const results = [
+  {
+    "group":"d5",
+    "threads":"1",
+    "mode":"detection",
+    "lines":"50.00",
+    "memory":"0.00",
+    "detection_accuracy":"2.55",
+    "correction_accuracy":"0.00",
+    "date":"2026-04-10",
+    "timings":[
+      50.0
+    ],
+    "comments":"OK"
+  }
+];
+
+export default results;
---
+++ championship/importer.js
@@ -0,0 +1,4 @@
+import results0 from "./2026_04_10.js";
+import results1 from "./data.js";
+
+export default [ results0, results1 ];
