c9a2c2a8f933cb23b585ceeabbb5bcad00ccd99c
Benoît Legat <benoit.legat@gmail.com>
2026-04-20 11:28:44 +0200

    2026_04_19

--- championship/data/importer.js
+++ championship/data/importer.js
@@ -4,5 +4,6 @@ import results2 from "./2026_04_14.js";
 import results3 from "./2026_04_15.js";
 import results4 from "./2026_04_16.js";
 import results5 from "./2026_04_17.js";
+import results6 from "./2026_04_19.js";
 
-export default [ results0, results1, results2, results3, results4, results5 ];
+export default [ results0, results1, results2, results3, results4, results5, results6 ];
